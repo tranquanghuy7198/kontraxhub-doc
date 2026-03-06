25a900aad495eed4f4fe567aa5cf6c592900837d
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2026-03-06 16:24:22 +0700

    feat: Add link to product label

--- src/components/product-label/index.tsx
+++ src/components/product-label/index.tsx
@@ -1,6 +1,10 @@
-import { PRODUCT_NAME } from "@site/src/constants";
+import { DOMAIN, PRODUCT_NAME } from "@site/src/constants";
 import "./product-label.css";
 
-const ProductLabel = () => <div className="product-label">{PRODUCT_NAME}</div>;
+const ProductLabel = () => (
+  <a href={DOMAIN} target="_blank" className="product-label">
+    {PRODUCT_NAME}
+  </a>
+);
 
 export default ProductLabel;
