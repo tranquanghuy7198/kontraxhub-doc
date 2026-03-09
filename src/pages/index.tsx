835f627f31558e1668db4f0603b4654019a1ded9
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2026-03-09 11:56:45 +0700

    feat: Do not use Layout in landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,8 @@
 import type { ReactNode } from "react";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
+// import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Landing from "@site/src/pages/landing";
 
 export default function Home(): ReactNode {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <Landing />
-    </Layout>
-  );
+  // const { siteConfig } = useDocusaurusContext();
+  return <Landing />;
 }
