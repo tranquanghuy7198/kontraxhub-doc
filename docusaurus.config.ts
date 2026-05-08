83c09867c64d9abca6a6e7cc6ce6c86750685598
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2026-05-08 18:10:06 +0700

    feat: Change backend URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -156,7 +156,7 @@ const config: Config = {
         route: "/dev-apis",
         showNavLink: true, // optional, default is true
         configuration: {
-          url: "https://chainforge-backend-smef.onrender.com/swagger-dev.json",
+          url: "https://undecided-audibly-reputable.ngrok-free.dev/swagger-dev.json",
         },
       } as ScalarOptions,
     ],
